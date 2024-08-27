9fe95894cee5b41028c1009c438dffea0f3287b8
anton1dev <anton.kuchmasov.dev@gmail.com>
2024-08-27 16:56:06 +0300

    added navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
+'use client'
 import Dashboard from "@/app/dashboard/page";
+import { useSession } from "next-auth/react";
 import Header from "./components/Header";
-import styles from './page.module.css'
 import SideMenu from "./components/SideMenu";
+import styles from './page.module.css';
+import Login from "./components/Login";
 
 export default function Home() {
+  const { data: session } = useSession();
+
   return (
     <main className={styles.main}>
-      <Header />
-      <SideMenu />
-      <Dashboard />
+      <Header /> {
+        session && (
+          <><SideMenu /><Dashboard /></>
+        )
+      }
+     <Login />
     </main >
   );
 }
