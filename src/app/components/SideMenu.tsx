c48a4cff2e49067e6ab76067819aa826b5503d34
anton1dev <anton.kuchmasov.dev@gmail.com>
2024-08-27 10:53:51 +0300

    initial

---
+++ src/app/components/SideMenu.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SideMenu = () => {
+  return (
+    <div>
+        <ul>
+            <li>Analytics</li>
+            <li>Settings</li>
+            <li>Profile</li>
+        </ul>
+    </div>
+  )
+}
+
+export default SideMenu
